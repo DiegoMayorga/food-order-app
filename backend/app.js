8bdbc4649fba05c1d1b0fde170a9aff6f78ff276
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-06 12:13:21 -0500

    Last changes. Using useActionState to replace the isLoading logic. The project is done.

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,9 @@ app.get('/meals', async (req, res) => {
 app.post('/orders', async (req, res) => {
   const orderData = req.body.order;
 
+  // 7 Agrego un delay de 1 segundo para simular una peticion lenta.
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   if (orderData === null || orderData.items === null || orderData.items.length === 0) {
     return res
       .status(400)
